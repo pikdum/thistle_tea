7db72e628c59730bf1ccf13bccdfa33612fbf1ed
pikdum <pikdum@kuudere.moe>
2024-06-04 03:05:33 -0500

    refactor: define opcodes

--- lib/game.ex
+++ lib/game.ex
@@ -8,14 +8,23 @@ defmodule ThistleTea.Game do
 
   import Binary, only: [split_at: 2, trim_trailing: 1]
 
+  @smsg_auth_challenge 0x1EC
+  @smg_auth_response 0x1EE
+
   @cmsg_auth_session 0x1ED
+  @cmsg_char_enum 0x037
 
   @impl ThousandIsland.Handler
   def handle_connection(socket, _state) do
     # send SMSG_AUTH_CHALLENGE
     seed = :crypto.strong_rand_bytes(4)
     Logger.info("[GameServer] Sending SMSG_AUTH_CHALLENGE with seed: #{inspect(seed)}")
-    ThousandIsland.Socket.send(socket, <<6::big-size(16), 0x1EC::little-size(16)>> <> seed)
+
+    ThousandIsland.Socket.send(
+      socket,
+      <<6::big-size(16), @smsg_auth_challenge::little-size(16)>> <> seed
+    )
+
     {:continue, %{seed: seed}}
   end
 
@@ -52,7 +61,7 @@ defmodule ThistleTea.Game do
 
       CryptoStorage.send_packet(
         crypto_pid,
-        0x1EE,
+        @smg_auth_response,
         <<0x0C, 0::little-size(32), 0, 0::little-size(32)>>,
         socket
       )
@@ -66,14 +75,14 @@ defmodule ThistleTea.Game do
 
   @impl ThousandIsland.Handler
   def handle_data(
-        <<header::bytes-size(6), _body::binary>>,
+        <<header::bytes-size(6), body::binary>>,
         _socket,
         state
       ) do
     case CryptoStorage.decrypt_header(state.crypto_pid, header) do
       <<size::big-size(16), opcode::little-size(32)>> ->
         Logger.info(
-          "[GameServer] Decrypted header: size: #{size}, opcode: #{inspect(opcode, base: :hex)}"
+          "[GameServer] #{inspect(opcode, base: :hex)}, size: #{size}, calculated_size: #{byte_size(body) + 4}"
         )
 
       other ->
