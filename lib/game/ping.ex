ead7845f8261180f0adbf97f65431c199168fdab
pikdum <pikdum@kuudere.moe>
2024-06-05 15:16:37 -0500

    refactor: extract out login + ping

---
+++ lib/game/ping.ex
@@ -0,0 +1,16 @@
+defmodule ThistleTea.Game.Ping do
+  defmacro __using__(_) do
+    quote do
+      @cmsg_ping 0x1DC
+      @smg_pong 0x1DD
+
+      @impl GenServer
+      def handle_cast({:handle_packet, @cmsg_ping, _size, body}, {socket, state}) do
+        <<sequence_id::little-size(32), latency::little-size(32)>> = body
+        Logger.info("[GameServer] CMSG_PING: sequence_id: #{sequence_id}, latency: #{latency}")
+        send_packet(@smg_pong, <<sequence_id::little-size(32)>>)
+        {:noreply, {socket, Map.put(state, :latency, latency)}}
+      end
+    end
+  end
+end
