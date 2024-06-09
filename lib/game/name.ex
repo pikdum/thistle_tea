345465d9a7a47c42120b358f6fd74eede31050a0
pikdum <pikdum@kuudere.moe>
2024-06-08 19:43:12 -0500

    fix: need to terminate strings with 0 byte

--- lib/game/name.ex
+++ lib/game/name.ex
@@ -8,12 +8,13 @@ defmodule ThistleTea.Game.Name do
       def handle_cast({:handle_packet, @cmsg_name_query, _size, body}, {socket, state}) do
         <<guid::little-size(64)>> = body
         [{^guid, character_name, realm_name, race, gender, class}] = :ets.lookup(:guid_name, guid)
-        Logger.info("[GameServer] CMSG_NAME_QUERY: #{guid} -> #{character_name}")
-        # TODO: this is either buggy or me running two clients is
+
+        Logger.info("[GameServer] CMSG_NAME_QUERY: #{character_name} <- #{guid}")
 
         send_packet(
           @smsg_name_query_response,
-          <<guid::little-size(64)>> <> character_name <> realm_name <> <<race, gender, class>>
+          <<guid::little-size(64)>> <>
+            character_name <> <<0>> <> realm_name <> <<0>> <> <<race, gender, class>>
         )
 
         {:noreply, {socket, state}}
