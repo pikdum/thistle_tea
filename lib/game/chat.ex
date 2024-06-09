e973505d001bf8c45b0e98e1cbec8ab186b2fe3d
pikdum <pikdum@kuudere.moe>
2024-06-09 01:41:36 -0500

    feat: working say messages

--- lib/game/chat.ex
+++ lib/game/chat.ex
@@ -6,6 +6,8 @@ defmodule ThistleTea.Game.Chat do
       @cmsg_messagechat 0x095
       @smsg_messagechat 0x096
 
+      @cmsg_join_channel 0x097
+
       @chat_type_say 0
       @chat_type_whisper 6
       @chat_type_channel 14
@@ -13,7 +15,7 @@ defmodule ThistleTea.Game.Chat do
       @impl GenServer
       def handle_cast({:handle_packet, @cmsg_messagechat, _size, body}, {socket, state}) do
         <<chat_type::little-size(32), language::little-size(32), rest::binary>> = body
-        Logger.info("[GameServer] CMSG_MESSAGECHAT: #{inspect(language)}")
+        Logger.info("[GameServer] CMSG_MESSAGECHAT: chat_type: #{inspect(chat_type)}")
 
         {target_player, rest} =
           case chat_type do
@@ -35,13 +37,19 @@ defmodule ThistleTea.Game.Chat do
               {nil, rest}
           end
 
+        # SizedCString = size + string + null byte
         {:ok, message, _rest} = parse_string(rest)
+        message_length = String.length(message) + 1
+
+        # hardcoded to universal language for now
+        language = 0
 
         case chat_type do
           @chat_type_say ->
             packet =
               <<chat_type::little-size(8), language::little-size(32), state.guid::little-size(64),
                 state.guid::little-size(64)>> <>
+                <<message_length::little-size(32)>> <>
                 message <>
                 <<
                   0,
@@ -51,12 +59,9 @@ defmodule ThistleTea.Game.Chat do
 
             Registry.dispatch(ThistleTea.PubSub, "logged_in", fn entries ->
               for {pid, _} <- entries do
-                Logger.info("[GameServer] CMSG_MESSAGECHAT: SAY: #{inspect(pid)}")
                 send(pid, {:send_packet, @smsg_messagechat, packet})
               end
             end)
-
-            Logger.info("[GameServer] CMSG_MESSAGECHAT: SAY: #{message}")
 
           @chat_type_whisper ->
             Logger.info("[GameServer] CMSG_MESSAGECHAT: WHISPER: #{target_player} -> #{message}")
@@ -67,6 +72,15 @@ defmodule ThistleTea.Game.Chat do
           _ ->
             Logger.info("[GameServer] CMSG_MESSAGECHAT: UNKNOWN: #{inspect(chat_type)}")
         end
+
+        {:noreply, {socket, state}}
+      end
+
+      @impl GenServer
+      def handle_cast({:handle_packet, @cmsg_join_channel, _size, body}, {socket, state}) do
+        {:ok, channel_name, rest} = parse_string(body)
+        {:ok, channel_password, _} = parse_string(rest)
+        Logger.info("[GameServer] CMSG_JOIN_CHANNEL: #{channel_name} -> #{channel_password}")
 
         {:noreply, {socket, state}}
       end
