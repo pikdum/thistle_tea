5d633e7afb51d325c92013ed11516400178964b2
pikdum <pikdum@kuudere.moe>
2024-06-11 20:42:13 -0500

    refactor: auth logging + account

--- config/config.exs
+++ config/config.exs
@@ -6,4 +6,14 @@ config :thistle_tea, ThistleTea.Mangos, database: "mangos0.sqlite"
 
 config :mnesia, dir: ~c".mnesia/#{Mix.env()}/#{node()}"
 
+config :logger, :default_formatter,
+  format: "[$level] $message $metadata\n",
+  metadata: [
+    :username,
+    :file,
+    :pid,
+    :mfa,
+    :error_code
+  ]
+
 import_config "#{config_env()}.exs"
