669cbf912717078791a83bcdd1231cfdc0806dfc
pikdum <pikdum@kuudere.moe>
2024-06-14 20:18:47 -0500

    refactor: add some abstractions

--- lib/game/login.ex
+++ lib/game/login.ex
@@ -6,6 +6,7 @@ defmodule ThistleTea.Game.Login do
       import ThistleTea.Game.UpdateObject
       import Bitwise, only: [<<<: 2, |||: 2]
       import ThistleTea.Util, only: [pack_guid: 1]
+      import ThistleTea.Character, only: [get_update_fields: 1]
 
       @cmsg_player_login 0x03D
       @smsg_login_verify_world 0x236
@@ -42,8 +43,9 @@ defmodule ThistleTea.Game.Login do
 
         send_packet(
           @smsg_login_verify_world,
-          <<c.map::little-size(32), c.x::little-float-size(32), c.y::little-float-size(32),
-            c.z::little-float-size(32), c.orientation::little-float-size(32)>>
+          <<c.map::little-size(32), c.movement.x::little-float-size(32),
+            c.movement.y::little-float-size(32), c.movement.z::little-float-size(32),
+            c.movement.orientation::little-float-size(32)>>
         )
 
         # needed for no white chatbox + keybinds
@@ -62,9 +64,10 @@ defmodule ThistleTea.Game.Login do
         # let's just init it to character's position for now
         send_packet(
           @smsg_bindpointupdate,
-          <<c.map::little-size(32), c.x::little-float-size(32), c.y::little-float-size(32),
-            c.z::little-float-size(32), c.orientation::little-float-size(32),
-            c.map::little-size(32), c.area::little-size(32)>>
+          <<c.map::little-size(32), c.movement.x::little-float-size(32),
+            c.movement.y::little-float-size(32), c.movement.z::little-float-size(32),
+            c.movement.orientation::little-float-size(32), c.map::little-size(32),
+            c.area::little-size(32)>>
         )
 
         # no tutorials
@@ -90,6 +93,7 @@ defmodule ThistleTea.Game.Login do
         chr_race = DBC.get_by(ChrRaces, id: c.race)
 
         # SMSG_TRIGGER_CINEMATIC
+        # TODO: on first login only
         if false do
           send_packet(
             @smsg_trigger_cinematic,
@@ -97,77 +101,15 @@ defmodule ThistleTea.Game.Login do
           )
         end
 
-        unit_display_id =
-          case(c.gender) do
-            0 -> chr_race.male_display
-            1 -> chr_race.female_display
-          end
-
-        mb = %{
-          # what is 0x71?: SELF ||| ALL ||| LIVING ||| HAS_POSITION
-          update_flag:
+        mb =
+          Map.put(
+            c.movement,
+            :update_flag,
             @update_flag_self ||| @update_flag_all ||| @update_flag_living |||
-              @update_flag_has_position,
-          movement_flags: 0,
-          position: {c.x, c.y, c.z, c.orientation},
-          fall_time: 0.0,
-          walk_speed: 1.0,
-          # run_speed: 7.0,
-          run_speed: 20.0,
-          run_back_speed: 4.5,
-          swim_speed: 0.0,
-          swim_back_speed: 0.0,
-          turn_rate: 3.1415
-        }
+              @update_flag_has_position
+          )
 
-        fields = %{
-          object_guid: c.id,
-          object_type: 25,
-          object_scale_x: 1.0,
-          unit_health: 100,
-          unit_power_1: 100,
-          unit_power_2: 100,
-          unit_power_3: 100,
-          unit_power_4: 100,
-          unit_power_5: 100,
-          unit_max_health: 100,
-          unit_max_power_1: 100,
-          unit_max_power_2: 100,
-          unit_max_power_3: 100,
-          unit_max_power_4: 100,
-          unit_max_power_5: 100,
-          unit_level: c.level,
-          unit_faction_template: 1,
-          unit_bytes_0: <<c.race, c.class, c.gender, 1>>,
-          unit_display_id: unit_display_id,
-          unit_native_display_id: unit_display_id,
-          player_flags: 0,
-          player_features: <<c.skin, c.face, c.hair_style, c.hair_color>>,
-          player_xp: 1,
-          player_next_level_xp: 100,
-          player_rest_state_experience: 100,
-          # TODO: handle empty equipment slot
-          player_visible_item_1_0: c.equipment.head.entry,
-          player_visible_item_2_0: c.equipment.neck.entry,
-          player_visible_item_3_0: c.equipment.shoulders.entry,
-          player_visible_item_4_0: c.equipment.body.entry,
-          player_visible_item_5_0: c.equipment.chest.entry,
-          player_visible_item_6_0: c.equipment.waist.entry,
-          player_visible_item_7_0: c.equipment.legs.entry,
-          player_visible_item_8_0: c.equipment.feet.entry,
-          player_visible_item_9_0: c.equipment.wrists.entry,
-          player_visible_item_10_0: c.equipment.hands.entry,
-          player_visible_item_11_0: c.equipment.finger1.entry,
-          player_visible_item_12_0: c.equipment.finger2.entry,
-          player_visible_item_13_0: c.equipment.trinket1.entry,
-          player_visible_item_14_0: c.equipment.trinket2.entry,
-          player_visible_item_15_0: c.equipment.back.entry,
-          player_visible_item_16_0: c.equipment.mainhand.entry,
-          # player_visible_item_17_0: c.equipment.offhand.entry,
-          # player_visible_item_18_0: c.equipment.ranged.entry,
-          player_visible_item_19_0: c.equipment.tabard.entry
-        }
-
+        fields = get_update_fields(c)
         packet = generate_packet(@update_type_create_object2, @object_type_player, fields, mb)
 
         # player logged in
@@ -199,8 +141,7 @@ defmodule ThistleTea.Game.Login do
           Map.merge(state, %{
             guid: character_guid,
             packed_guid: pack_guid(character_guid),
-            character: c,
-            unit_display_id: unit_display_id
+            character: c
           })
 
         {:noreply, {socket, new_state}, socket.read_timeout}
--- lib/model/character.ex
+++ lib/model/character.ex
@@ -13,16 +13,13 @@ defmodule ThistleTea.Character do
       :hair_color,
       :facial_hair,
       :level,
-      # TODO: maybe orientation should be elsewhere?
       :area,
       :map,
-      :x,
-      :y,
-      :z,
-      :orientation,
-      # TODO: is this needed?
+      # TODO: is outfit_id needed?
       :outfit_id,
-      :equipment
+      :unit_display_id,
+      :equipment,
+      :movement
     ],
     index: [:account_id, :name],
     type: :ordered_set,
@@ -38,6 +35,59 @@ defmodule ThistleTea.Character do
       {:limit, true} -> {:error, :character_limit}
       {:error, reason} -> {:error, reason}
     end
+  end
+
+  def get_update_fields(c) do
+    # TODO: maybe have these part of character itself
+    # TODO: add more of these to the data model
+    %{
+      object_guid: c.id,
+      # TODO: what is 25?
+      object_type: 25,
+      object_scale_x: 1.0,
+      unit_health: 100,
+      unit_power_1: 100,
+      unit_power_2: 100,
+      unit_power_3: 100,
+      unit_power_4: 100,
+      unit_power_5: 100,
+      unit_max_health: 100,
+      unit_max_power_1: 100,
+      unit_max_power_2: 100,
+      unit_max_power_3: 100,
+      unit_max_power_4: 100,
+      unit_max_power_5: 100,
+      unit_level: c.level,
+      unit_faction_template: 1,
+      unit_bytes_0: <<c.race, c.class, c.gender, 1>>,
+      unit_display_id: c.unit_display_id,
+      unit_native_display_id: c.unit_display_id,
+      player_flags: 0,
+      player_features: <<c.skin, c.face, c.hair_style, c.hair_color>>,
+      player_xp: 1,
+      player_next_level_xp: 100,
+      player_rest_state_experience: 100,
+      # TODO: handle empty equipment slot
+      player_visible_item_1_0: c.equipment.head.entry,
+      player_visible_item_2_0: c.equipment.neck.entry,
+      player_visible_item_3_0: c.equipment.shoulders.entry,
+      player_visible_item_4_0: c.equipment.body.entry,
+      player_visible_item_5_0: c.equipment.chest.entry,
+      player_visible_item_6_0: c.equipment.waist.entry,
+      player_visible_item_7_0: c.equipment.legs.entry,
+      player_visible_item_8_0: c.equipment.feet.entry,
+      player_visible_item_9_0: c.equipment.wrists.entry,
+      player_visible_item_10_0: c.equipment.hands.entry,
+      player_visible_item_11_0: c.equipment.finger1.entry,
+      player_visible_item_12_0: c.equipment.finger2.entry,
+      player_visible_item_13_0: c.equipment.trinket1.entry,
+      player_visible_item_14_0: c.equipment.trinket2.entry,
+      player_visible_item_15_0: c.equipment.back.entry,
+      player_visible_item_16_0: c.equipment.mainhand.entry,
+      # player_visible_item_17_0: c.equipment.offhand.entry,
+      # player_visible_item_18_0: c.equipment.ranged.entry,
+      player_visible_item_19_0: c.equipment.tabard.entry
+    }
   end
 
   def character_exists?(name) do
