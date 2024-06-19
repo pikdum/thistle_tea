1d0cfa2009a7075a2761a4562b05cb6f24ed64af
pikdum <pikdum@kuudere.moe>
2024-06-19 04:33:40 -0500

    chore: improve deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 set -euxo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
@@ -6,3 +6,5 @@ SERVER="opc@shimarin.kuudere.moe"
 
 # exclude _build and .git
 rsync -avz --exclude '_build' --exclude '.git' --exclude 'deps' $SCRIPT_DIR/ $SERVER:~/thistle_tea/
+
+ssh $SERVER "cd ~/thistle_tea && bash -l -c './run.sh'"
---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+export PATH="/home/opc/.local/share/mise/shims:$PATH"
+export MIX_ENV=prod
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+cd "$SCRIPT_DIR"
+_build/prod/rel/thistle_tea/bin/thistle_tea stop || true
+rm -rf _build
+mix release
+_build/prod/rel/thistle_tea/bin/thistle_tea daemon
