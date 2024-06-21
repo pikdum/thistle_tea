fb4e18551e6bd444c0ea56a0d2967b15e951dfeb
pikdum <pikdum@kuudere.moe>
2024-06-20 20:52:01 -0500

    chore: turn off ecto logging

--- config/config.exs
+++ config/config.exs
@@ -1,8 +1,8 @@
 import Config
 
 config :thistle_tea, ecto_repos: [ThistleTea.DBC, ThistleTea.Mangos]
-config :thistle_tea, ThistleTea.DBC, database: "vanilla_dbcs.sqlite"
-config :thistle_tea, ThistleTea.Mangos, database: "mangos0.sqlite"
+config :thistle_tea, ThistleTea.DBC, database: "vanilla_dbcs.sqlite", log: false
+config :thistle_tea, ThistleTea.Mangos, database: "mangos0.sqlite", log: false
 
 config :mnesia, dir: ~c".mnesia/#{Mix.env()}/#{node()}"
 
