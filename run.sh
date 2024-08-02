f7782cf7e4edc968c478071f49d66345e9c1c922
pikdum <pikdum@kuudere.moe>
2024-08-02 16:51:58 -0500

    chore: update deps on deploy

--- run.sh
+++ run.sh
@@ -8,5 +8,6 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_DIR"
 _build/prod/rel/thistle_tea/bin/thistle_tea stop || true
 rm -rf _build
+mix deps.get
 mix release
 _build/prod/rel/thistle_tea/bin/thistle_tea daemon
