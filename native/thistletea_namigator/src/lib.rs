41aec2f805ad2bd00c6c17f033f0a6003117c743
pikdum <pikdum@kuudere.moe>
2024-09-11 18:51:18 -0500

    wip: namigator refactoring

--- native/thistletea_namigator/src/lib.rs
+++ native/thistletea_namigator/src/lib.rs
@@ -2,7 +2,7 @@ use namigator::raw::{build_bvh, build_map, bvh_files_exist, map_files_exist, Pat
 use namigator::vanilla::{Map, VanillaMap};
 use namigator::Vector3d;
 use once_cell::sync::Lazy;
-use rustler::{Encoder, Env, NifResult, Term};
+use rustler::NifResult;
 use std::collections::HashMap;
 use std::path::PathBuf;
 use std::sync::Mutex;
@@ -11,8 +11,7 @@ static PATHFINDING_MAPS: Lazy<Mutex<Option<PathfindingMaps>>> = Lazy::new(|| Mut
 
 #[derive(Debug)]
 pub struct PathfindingMaps {
-    maps: HashMap<Map, VanillaMap>,
-    raw_maps: HashMap<String, PathfindMap>,
+    maps: HashMap<u32, PathfindMap>,
 }
 
 impl PathfindingMaps {
@@ -21,8 +20,7 @@ impl PathfindingMaps {
         let output = PathBuf::from(out_dir);
 
         println!("Building and using maps for pathfind from data directory '{}' and outputting to '{}'. This may take a while.", data_path.display(), output.display());
-        let mut m = HashMap::new();
-        let mut raw_m = HashMap::new();
+        let mut maps = HashMap::new();
 
         let threads = {
             let t = std::thread::available_parallelism().unwrap().get() as u32;
@@ -42,66 +40,68 @@ impl PathfindingMaps {
             println!("Gameobjects already built.");
         }
 
-        const MAP: Map = Map::DevelopmentLand;
-        const MAP_FILE_NAME: &str = "development";
+        // Hardcoded list of Map variants to process
+        const MAPS_TO_PROCESS: &[Map] = &[
+            Map::EasternKingdoms,
+            Map::Kalimdor,
+            Map::DevelopmentLand,
+            // Add other Map variants you want to process
+        ];
 
-        if !map_files_exist(&output, MAP_FILE_NAME).map_err(|e| e.to_string())? {
-            println!("Building map {MAP} ({})", MAP_FILE_NAME);
-            build_map(&data_path, &output, MAP_FILE_NAME, "", threads)
-                .map_err(|e| e.to_string())?;
-            println!("Finished building {MAP} ({})", MAP_FILE_NAME);
-        } else {
-            println!("{MAP} ({}) already built.", MAP_FILE_NAME);
+        for &map in MAPS_TO_PROCESS {
+            let map_file_name = map.directory_name();
+            let map_id = map.as_int();
+
+            if !map_files_exist(&output, map_file_name).map_err(|e| e.to_string())? {
+                println!("Building map {:?} ({map_file_name})", map);
+                build_map(&data_path, &output, map_file_name, "", threads)
+                    .map_err(|e| e.to_string())?;
+                println!("Finished building {:?} ({map_file_name})", map);
+            } else {
+                println!("{:?} ({map_file_name}) already built.", map);
+            }
+
+            let mut vanilla_map = VanillaMap::new(&output, map).map_err(|e| e.to_string())?;
+            vanilla_map.load_all_adts().map_err(|e| e.to_string())?;
+
+            let mut pathfind_map =
+                PathfindMap::new(&output, map_file_name).map_err(|e| e.to_string())?;
+            pathfind_map.load_all_adts().map_err(|e| e.to_string())?;
+
+            maps.insert(map_id, pathfind_map);
+            println!("PathfindMap inserted with key: {}", map_id);
         }
-
-        let mut v = VanillaMap::build_gameobjects_and_map(&data_path, &output, MAP, threads)
-            .map_err(|e| e.to_string())?;
-        v.load_all_adts().map_err(|e| e.to_string())?;
-        m.insert(MAP, v);
-        println!("VanillaMap inserted with key: {:?}", MAP);
-
-        let mut raw_map = PathfindMap::new(&output, MAP_FILE_NAME).map_err(|e| e.to_string())?;
-        raw_map.load_all_adts().map_err(|e| e.to_string())?;
-        raw_m.insert(MAP_FILE_NAME.to_string(), raw_map);
-        println!("PathfindMap inserted with key: {}", MAP_FILE_NAME);
 
         println!("Finished setting up maps");
 
-        Ok(Self {
-            maps: m,
-            raw_maps: raw_m,
-        })
-    }
-
-    pub fn get(&self, map: &Map) -> Option<&VanillaMap> {
-        self.maps.get(&map)
+        Ok(Self { maps })
     }
 
     pub fn get_zone_and_area(
         &self,
-        map_name: &str,
+        map_id: u32,
         x: f32,
         y: f32,
         z: f32,
     ) -> Result<(u32, u32), String> {
-        if let Some(map) = self.raw_maps.get(map_name) {
+        if let Some(map) = self.maps.get(&map_id) {
             map.get_zone_and_area(x, y, z).map_err(|e| e.to_string())
         } else {
-            Err(format!("Map '{}' not found", map_name))
+            Err(format!("Map with ID '{}' not found", map_id))
         }
     }
 
     pub fn find_random_point_around_circle(
         &self,
-        map_name: &str,
+        map_id: u32,
         start: Vector3d,
         radius: f32,
     ) -> Result<Vector3d, String> {
-        if let Some(map) = self.raw_maps.get(map_name) {
+        if let Some(map) = self.maps.get(&map_id) {
             map.find_random_point_around_circle(start, radius)
                 .map_err(|e| e.to_string())
         } else {
-            Err(format!("Map '{}' not found", map_name))
+            Err(format!("Map with ID '{}' not found", map_id))
         }
     }
 }
@@ -123,35 +123,10 @@ fn build(wow_dir: String, out_dir: String) -> NifResult<bool> {
 }
 
 #[rustler::nif]
-fn get_map(map_name: String) -> NifResult<bool> {
-    println!("get_map called with map_name: {}", map_name);
+fn get_zone_and_area(map_id: u32, x: f32, y: f32, z: f32) -> NifResult<Option<(u32, u32)>> {
     let global_maps = PATHFINDING_MAPS.lock().unwrap();
     match &*global_maps {
-        Some(maps) => {
-            let map = match map_name.as_str() {
-                "development" => Map::DevelopmentLand,
-                // Add other map variants as needed
-                _ => {
-                    println!("Unknown map name: {}", map_name);
-                    return Ok(false);
-                }
-            };
-            let result = maps.get(&map).is_some();
-            println!("Map {} found: {}", map_name, result);
-            Ok(result)
-        }
-        None => {
-            println!("PathfindingMaps not initialized");
-            Ok(false)
-        }
-    }
-}
-
-#[rustler::nif]
-fn get_zone_and_area(map_name: String, x: f32, y: f32, z: f32) -> NifResult<Option<(u32, u32)>> {
-    let global_maps = PATHFINDING_MAPS.lock().unwrap();
-    match &*global_maps {
-        Some(maps) => match maps.get_zone_and_area(&map_name, x, y, z) {
+        Some(maps) => match maps.get_zone_and_area(map_id, x, y, z) {
             Ok((zone, area)) => Ok(Some((zone, area))),
             Err(_) => Ok(None),
         },
@@ -161,7 +136,7 @@ fn get_zone_and_area(map_name: String, x: f32, y: f32, z: f32) -> NifResult<Opti
 
 #[rustler::nif]
 fn find_random_point_around_circle(
-    map_name: String,
+    map_id: u32,
     x: f32,
     y: f32,
     z: f32,
@@ -170,8 +145,8 @@ fn find_random_point_around_circle(
     let global_maps = PATHFINDING_MAPS.lock().unwrap();
     match &*global_maps {
         Some(maps) => {
-            let start = Vector3d { x: x, y: y, z: z };
-            match maps.find_random_point_around_circle(&map_name, start, radius) {
+            let start = Vector3d { x, y, z };
+            match maps.find_random_point_around_circle(map_id, start, radius) {
                 Ok(point) => Ok(Some((point.x, point.y, point.z))),
                 Err(_) => Ok(None),
             }
