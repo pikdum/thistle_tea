ab228a593399080fdbf20da300df725457ac0970
pikdum <pikdum@users.noreply.github.com>
2024-10-19 19:57:15 -0500

    refactor: use System.get_env/2

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,3 +1,3 @@
 import Config
-game_server = System.get_env("GAME_SERVER") || "localhost"
-config :thistle_tea, :game_server, game_server
+
+config :thistle_tea, :game_server, System.get_env("GAME_SERVER", "localhost")
