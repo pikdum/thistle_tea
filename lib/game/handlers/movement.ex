b073c2a39148592449a1a463c83f3f9a59e694a8
pikdum <pikdum@kuudere.moe>
2025-01-07 04:54:02 -0600

    fix: ignore movement while teleport is queued
    tested by trying to teleport while jumping off blackrock mountain

--- lib/game/handlers/movement.ex
+++ lib/game/handlers/movement.ex
@@ -68,7 +68,7 @@ defmodule ThistleTea.Game.Movement do
     end
   end
 
-  def handle_packet(msg, body, state)
+  def handle_packet(msg, body, %{ready: true} = state)
       when msg in [
              @msg_move_start_forward,
              @msg_move_start_backward,
@@ -136,6 +136,10 @@ defmodule ThistleTea.Game.Movement do
       end
     end
 
+    {:continue, state}
+  end
+
+  def handle_packet(_msg, _body, state) do
     {:continue, state}
   end
 end
