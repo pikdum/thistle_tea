238c0b05257bada2f84e9a99c6e49cee817211e2
pikdum <pikdum@kuudere.moe>
2025-09-15 20:17:22 -0500

    chore: add some extra fields

--- lib/game/field_struct.ex
+++ lib/game/field_struct.ex
@@ -1,18 +1,19 @@
 defmodule ThistleTea.Game.FieldStruct do
   defmacro __using__(fields) do
     quote do
+      @metadata Map.new(unquote(fields))
       defstruct unquote(Keyword.keys(fields))
 
       def offset(field), do: get_elem(field, 0)
       def size(field), do: get_elem(field, 1)
       def type(field), do: get_elem(field, 2)
+      def metadata(field), do: Map.get(@metadata, field)
+      def metadata, do: @metadata
 
-      # helper that can simplify the above 3 functions
       defp get_elem(field, index) do
-        case unquote(fields)[field] do
-          nil -> nil
-          tuple -> elem(tuple, index)
-        end
+        @metadata
+        |> Map.get(field)
+        |> elem(index)
       end
     end
   end
