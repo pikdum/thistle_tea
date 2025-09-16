989264e73a69a26d8bc7fcac7bbd7072471b4516
pikdum <pikdum@kuudere.moe>
2025-09-15 23:44:45 -0500

    wip: format

--- lib/game/entities/data/dynamic_object.ex
+++ lib/game/entities/data/dynamic_object.ex
@@ -3,9 +3,9 @@ defmodule ThistleTea.Game.Entities.Data.DynamicObject do
     caster: {0x0006, 2, :guid},
     bytes: {0x0008, 1, :bytes},
     spell_id: {0x0009, 1, :int},
-    radius: {0x000a, 1, :float},
-    pos_x: {0x000b, 1, :float},
-    pos_y: {0x000c, 1, :float},
-    pos_z: {0x000d, 1, :float},
-    facing: {0x000e, 1, :float}
+    radius: {0x000A, 1, :float},
+    pos_x: {0x000B, 1, :float},
+    pos_y: {0x000C, 1, :float},
+    pos_z: {0x000D, 1, :float},
+    facing: {0x000E, 1, :float}
 end
