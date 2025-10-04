bdca418b6c8a03e0aeba4e0361bb6fafb926ac25
pikdum <pikdum@kuudere.moe>
2025-10-03 22:19:29 -0500

    chore: update dependencies

--- mix.exs
+++ mix.exs
@@ -35,14 +35,14 @@ defmodule ThistleTea.MixProject do
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:telemetry, "~> 1.0"},
       {:bitmap, "~> 1.0"},
-      {:rustler, "~> 0.36.0"},
-      {:phoenix, "~> 1.8.0"},
+      {:rustler, "~> 0.36"},
+      {:phoenix, "~> 1.8"},
       {:phoenix_html, "~> 4.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 1.1.0"},
-      {:phoenix_live_dashboard, "~> 0.8.3"},
+      {:phoenix_live_view, "~> 1.1"},
+      {:phoenix_live_dashboard, "~> 0.8"},
       {:esbuild, "~> 0.10", runtime: Mix.env() == :dev},
-      {:tailwind, "~> 0.2.0", runtime: Mix.env() == :dev},
+      {:tailwind, "~> 0.2", runtime: Mix.env() == :dev},
       {:heroicons,
        github: "tailwindlabs/heroicons",
        tag: "v2.2.0",
@@ -59,8 +59,8 @@ defmodule ThistleTea.MixProject do
        ref: "002b45269e69036b3a028cfd94d77b78c8a8a0ad",
        only: [:dev, :test],
        runtime: false},
-      {:nx, "~> 0.10.0"},
-      {:evision, "~> 0.2.9"}
+      {:nx, "~> 0.10"},
+      {:evision, "~> 0.2"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
