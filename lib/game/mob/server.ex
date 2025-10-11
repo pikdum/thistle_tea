396314384da84809522a632edf0bdac925edb1d2
pikdum <pikdum@kuudere.moe>
2025-10-10 23:57:07 -0500

    wip: rough but working linear movement splines

--- lib/game/mob/server.ex
+++ lib/game/mob/server.ex
@@ -2,7 +2,10 @@ defmodule ThistleTea.Game.Mob.Server do
   use GenServer
 
   alias ThistleTea.Game.Entity
+  alias ThistleTea.Game.Message.SmsgMonsterMove
   alias ThistleTea.Game.Mob
+
+  @smsg_monster_move 0x0DD
 
   def start_link(%Mob.Data{} = state) do
     GenServer.start_link(__MODULE__, state)
@@ -19,6 +22,39 @@ defmodule ThistleTea.Game.Mob.Server do
   def handle_cast({:send_update_to, pid}, state) do
     packet = Entity.Core.update_packet(state)
     GenServer.cast(pid, {:send_update_packet, packet})
+    {:noreply, state}
+  end
+
+  @impl GenServer
+  def handle_cast({:move_to, x, y, z}, state) do
+    {x0, y0, z0, _o} = state.movement_block.position
+    map = state.internal.map
+    guid = state.object.guid
+
+    nearby_players = SpatialHash.query(:players, map, x0, y0, z0, 250)
+
+    path =
+      case ThistleTea.Pathfinding.find_path(map, {x0, y0, z0}, {x, y, z}) do
+        [{x0, y0, z0} | rest] -> rest
+        path -> path
+      end
+
+    move = %SmsgMonsterMove{
+      guid: guid,
+      spline_point: {x0, y0, z0},
+      spline_id: 0,
+      move_type: 0,
+      spline_flags: 0x100,
+      duration: 1000,
+      splines: path
+    }
+
+    payload = SmsgMonsterMove.to_binary(move)
+
+    for {_guid, pid, _distance} <- nearby_players do
+      GenServer.cast(pid, {:send_packet, @smsg_monster_move, payload})
+    end
+
     {:noreply, state}
   end
 
