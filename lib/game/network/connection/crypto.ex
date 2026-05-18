2f5406b77fa3dc22fa68fa5a5ebc5bfec4ce027b
pikdum <me@pikdum.dev>
2026-05-18 17:58:20 -0500

    perf: optimize header crypto
    Benchee results from temporary benchmark:\n\n- encrypt 4-byte header: 3.28M ips vs 2.26M ips, 305ns vs 443ns avg, 520B vs 976B memory\n- decrypt 6-byte header: 2.13M ips vs 1.60M ips, 469ns vs 626ns avg, 752B vs 1352B memory\n\nVerified with temporary equivalence tests and in-game load.

--- lib/game/network/connection/crypto.ex
+++ lib/game/network/connection/crypto.ex
@@ -6,21 +6,8 @@ defmodule ThistleTea.Game.Network.Connection.Crypto do
   end
 
   def decrypt_header(%Connection{binary_stream: <<header::bytes-size(6), rest::binary>>} = conn) do
-    initial_acc = {<<>>, %{recv_i: conn.recv_i, recv_j: conn.recv_j}}
-
-    {decrypted_header, crypto_state} =
-      Enum.reduce(
-        :binary.bin_to_list(header),
-        initial_acc,
-        fn byte, {header, crypt} ->
-          recv_i = rem(crypt.recv_i, byte_size(conn.session_key))
-          x = Bitwise.bxor(byte - crypt.recv_j, :binary.at(conn.session_key, recv_i))
-          <<truncated_x>> = <<x::little-size(8)>>
-          {header <> <<truncated_x>>, %{recv_i: recv_i + 1, recv_j: byte}}
-        end
-      )
-
-    new_conn = Map.merge(conn, crypto_state)
+    {decrypted_header, recv_i, recv_j} =
+      decrypt_bytes(header, conn.session_key, byte_size(conn.session_key), conn.recv_i, conn.recv_j, <<>>)
 
     rest_size = byte_size(rest)
     <<size::big-size(16), _opcode::little-size(32)>> = decrypted_header
@@ -28,28 +15,38 @@ defmodule ThistleTea.Game.Network.Connection.Crypto do
     if rest_size < size - 4 do
       {:error, conn, :not_enough_data}
     else
-      {:ok, new_conn, decrypted_header}
+      {:ok, %{conn | recv_i: recv_i, recv_j: recv_j}, decrypted_header}
     end
   end
 
   def decrypt_header(%Connection{} = conn), do: {:error, conn, :not_enough_data}
 
   def encrypt_header(%Connection{} = conn, header) do
-    initial_acc = {<<>>, %{send_i: conn.send_i, send_j: conn.send_j}}
+    {encrypted_header, send_i, send_j} =
+      encrypt_bytes(header, conn.session_key, byte_size(conn.session_key), conn.send_i, conn.send_j, <<>>)
 
-    {encrypted_header, crypt_state} =
-      Enum.reduce(
-        :binary.bin_to_list(header),
-        initial_acc,
-        fn byte, {header, crypt} ->
-          send_i = rem(crypt.send_i, byte_size(conn.session_key))
-          x = Bitwise.bxor(byte, :binary.at(conn.session_key, send_i)) + crypt.send_j
-          <<truncated_x>> = <<x::little-size(8)>>
-          {header <> <<truncated_x>>, %{send_i: send_i + 1, send_j: truncated_x}}
-        end
-      )
+    {:ok, %{conn | send_i: send_i, send_j: send_j}, encrypted_header}
+  end
 
-    new_conn = Map.merge(conn, crypt_state)
-    {:ok, new_conn, encrypted_header}
+  defp decrypt_bytes(<<>>, _session_key, _key_size, recv_i, recv_j, acc) do
+    {acc, recv_i, recv_j}
+  end
+
+  defp decrypt_bytes(<<byte, rest::binary>>, session_key, key_size, recv_i, recv_j, acc) do
+    key_index = rem(recv_i, key_size)
+    x = Bitwise.bxor(byte - recv_j, :binary.at(session_key, key_index))
+    <<truncated_x>> = <<x::little-size(8)>>
+    decrypt_bytes(rest, session_key, key_size, key_index + 1, byte, <<acc::binary, truncated_x>>)
+  end
+
+  defp encrypt_bytes(<<>>, _session_key, _key_size, send_i, send_j, acc) do
+    {acc, send_i, send_j}
+  end
+
+  defp encrypt_bytes(<<byte, rest::binary>>, session_key, key_size, send_i, send_j, acc) do
+    key_index = rem(send_i, key_size)
+    x = Bitwise.bxor(byte, :binary.at(session_key, key_index)) + send_j
+    <<truncated_x>> = <<x::little-size(8)>>
+    encrypt_bytes(rest, session_key, key_size, key_index + 1, truncated_x, <<acc::binary, truncated_x>>)
   end
 end
