c1913d62b5a42a8ff5d23e051835f9512aa5a961
pikdum <me@pikdum.dev>
2026-05-18 19:37:22 -0500

    chore: don't deactivate cells for now
    will reimplement later after more thought, current approach is buggy

--- lib/game/world/system/cell_activator.ex
+++ lib/game/world/system/cell_activator.ex
@@ -4,7 +4,6 @@ defmodule ThistleTea.Game.World.System.CellActivator do
   """
   use GenServer
 
-  alias ThistleTea.Game.World
   alias ThistleTea.Game.World.Loader
   alias ThistleTea.Game.World.SpatialHash
 
@@ -37,24 +36,12 @@ defmodule ThistleTea.Game.World.System.CellActivator do
   @impl GenServer
   def handle_info(:poll, %__MODULE__{cells: old_cells} = state) do
     cells = player_cells() |> expand_cells()
-
-    occupied_cells =
-      MapSet.union(
-        SpatialHash.cells(:mobs),
-        SpatialHash.cells(:game_objects)
-      )
-
     to_add = MapSet.difference(cells, old_cells)
-    to_remove = MapSet.difference(occupied_cells, cells)
-
     :ok = start_cells(to_add)
-    :ok = stop_cells(to_remove)
-
     {:noreply, %{state | cells: cells}}
   end
 
   defp start_cells(cells), do: Enum.each(cells, &start_cell/1)
-  defp stop_cells(cells), do: Enum.each(cells, &stop_cell/1)
 
   defp start_cell(cell) do
     Logger.debug("Activating cell: #{inspect(cell)}")
@@ -63,22 +50,6 @@ defmodule ThistleTea.Game.World.System.CellActivator do
       Loader.Mob.load(cell)
       Loader.GameObject.load(cell)
     end)
-  end
-
-  defp stop_cell(cell) do
-    Logger.debug("Deactivating cell: #{inspect(cell)}")
-
-    stop_entities(:mobs, cell)
-    stop_entities(:game_objects, cell)
-  end
-
-  defp stop_entities(table, cell) do
-    SpatialHash.entities(table, cell)
-    |> Enum.each(&stop_entity/1)
-  end
-
-  defp stop_entity({_cell, guid}) do
-    World.stop_entity(guid)
   end
 
   defp player_cells do
