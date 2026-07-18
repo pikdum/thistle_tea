68553a23d45da7c69756607a050cc1200ca8bcab
pikdum <me@pikdum.dev>
2026-07-18 02:11:22 -0500

    test(talents): cover dependent ability reset

--- test/game/player/talents_test.exs
+++ test/game/player/talents_test.exs
@@ -14,17 +14,43 @@ defmodule ThistleTea.Game.Player.TalentsTest do
   setup do
     talent_id = System.unique_integer([:positive, :monotonic])
     spell_id = talent_id + 100_000
-    talent = %TalentData{id: talent_id, tab_id: 1, rank_spell_ids: [spell_id]}
+    dependent_spell_id = spell_id + 1
+    talent = %TalentData{id: talent_id, tab_id: 1, tier: 0, rank_spell_ids: [spell_id]}
+    previous_tabs = :ets.lookup(TalentLoader, {:tabs, 1})
 
     :ets.insert(TalentLoader, {{:talent, talent_id}, talent})
     :ets.insert(TalentLoader, {{:by_spell, spell_id}, {talent_id, 1, 0}})
+    :ets.insert(TalentLoader, {{:dependent_spells, spell_id}, [dependent_spell_id]})
+    :ets.insert(TalentLoader, {{:tabs, 1}, [1]})
 
     on_exit(fn ->
       :ets.delete(TalentLoader, {:talent, talent_id})
       :ets.delete(TalentLoader, {:by_spell, spell_id})
+      :ets.delete(TalentLoader, {:dependent_spells, spell_id})
+
+      case previous_tabs do
+        [] -> :ets.delete(TalentLoader, {:tabs, 1})
+        entries -> :ets.insert(TalentLoader, entries)
+      end
     end)
 
-    %{spell_id: spell_id}
+    %{dependent_spell_id: dependent_spell_id, spell_id: spell_id, talent_id: talent_id}
+  end
+
+  describe "learn/3 and reset/1" do
+    test "learns and resets talent-dependent abilities", context do
+      state = state_without_spells()
+
+      learned = Talents.learn(state, context.talent_id, 0)
+
+      assert Enum.sort(learned.character.internal.spells) ==
+               Enum.sort([context.spell_id, context.dependent_spell_id])
+
+      reset = Talents.reset(learned)
+
+      assert reset.character.internal.spells == []
+      assert reset.character.internal.spellbook == %{}
+    end
   end
 
   describe "reset_if_overbudget/2" do
@@ -54,6 +80,20 @@ defmodule ThistleTea.Game.Player.TalentsTest do
       unit: %Unit{level: level, auras: []},
       player: %Player{character_points1: 0},
       internal: %Internal{spells: [spell_id], spellbook: %{spell_id => %Spell{id: spell_id}}}
+    }
+
+    %{character: character}
+  end
+
+  defp state_without_spells do
+    id = System.unique_integer([:positive, :monotonic])
+
+    character = %Character{
+      id: id,
+      object: %Object{guid: id},
+      unit: %Unit{race: 1, class: 1, level: 10, auras: []},
+      player: %Player{skills: %{}},
+      internal: %Internal{spells: [], spellbook: %{}}
     }
 
     %{character: character}
